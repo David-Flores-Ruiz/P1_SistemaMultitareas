00be559070e9ed677bcb90d6a281a0d7f1ec12e9
rgpacas8 <ie706818@iteso.mx>
2019-09-29 20:55:02 -0500

    Lectura de una tecla con case

--- main.c
+++ main.c
@@ -54,13 +54,19 @@ int main(void) {
 /********************************************************************************************/
 
 	uint32_t PTC_4  = 0;	// Data Available
-
 	uint32_t PTD_0  = 0;	//	--- LSB
 	uint32_t PTD_1  = 0;	//			---	Bits de "ABCD"
 	uint32_t PTD_2  = 0;	//			--- salida del codificador 74c922
 	uint32_t PTD_3  = 0;	//  --- MSB
 	uint32_t total_input = 0;	// "DCBA" puede tener valores de 0 - 15
+	uint32_t tecla_presionada = 0;
 
+	/*
+	 * 1 2 3 A
+	 * 4 5 6 B
+	 * 7 8 9 C
+	 * * 0 # D
+	 */
 
 	while (1) {
 
@@ -78,18 +84,50 @@ int main(void) {
 /********************************************************************************************/
 			switch (total_input) {
 			case 0:
-				encender_LED(RED_ON);		// Led RED
-				delay(3000);
+				tecla_presionada='1';
 				break;
 			case 1:
-				encender_LED(BLUE_ON);		// Led BLUE
-				delay(3000);
+				tecla_presionada='2';
 				break;
 			case 2:
-				encender_LED(GREEN_ON);		// Led GREEN
-				delay(3000);
+				tecla_presionada='3';
 				break;
-
+			case 3:
+				tecla_presionada='A';
+				break;
+			case 4:
+				tecla_presionada='4';
+				break;
+			case 5:
+				tecla_presionada='5';
+				break;
+			case 6:
+				tecla_presionada='6';
+				break;
+			case 7:
+				tecla_presionada='B';
+				break;
+			case 8:
+				tecla_presionada='7';
+				break;
+			case 9:
+				tecla_presionada='8';
+				break;
+			case 10:
+				tecla_presionada='9';
+				break;
+			case 11:
+				tecla_presionada='C';
+				break;
+			case 12:
+				tecla_presionada='*';
+				break;
+			case 13:
+				tecla_presionada='#';
+				break;
+			case 14:
+				tecla_presionada='D';
+				break;
 			default:
 				encender_LED(RED_ON);
 				encender_LED(BLUE_ON);	// Led WHITE
@@ -104,7 +142,7 @@ int main(void) {
 			PTD_2  = 0;
 			PTD_3  = 0;
 			total_input = 0;
-		}
+		}//end if
 	}//end while(1)
 
 
