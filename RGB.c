6721a605479da816fa08cdcf173f03e8669c78b9
rgpacas8 <43006093+rgpacas8@users.noreply.github.com>
2019-10-02 17:05:15 -0500

    Contraseña

--- RGB.c
+++ RGB.c
@@ -183,25 +183,7 @@ uint8_t apagar_LED(color_OFF color_RGB) {
 	}
 	return (TRUE);
 }
-void RGB_init(void){
-	/**	Configurar el Clock Gating de los perifericos GPIO a utilizar */
-	GPIO_clock_gating( GPIO_B);	// led azul y rojo
-	GPIO_clock_gating( GPIO_E);	// led verde
-	gpio_pin_control_register_t led_config   = GPIO_MUX1;	// 100 de GPIO
-	/** OUTPUT: Configurar como LED RGB (GPIO) + safe value + como output */
 
-	GPIO_pin_control_register( GPIO_B, bit_21, &led_config);  // ledBlue  - pin 21 GPIO
-		GPIO_pin_control_register( GPIO_B, bit_22, &led_config);  // ledRed   - pin 22 GPIO
-		GPIO_pin_control_register( GPIO_E, bit_26, &led_config);  // ledGreen - pin 26 GPIO
-
-		GPIO_set_pin( GPIO_B, bit_22);	// OFF - 1 RED
-		GPIO_set_pin( GPIO_E, bit_26);	// OFF - 1 GREEN
-		GPIO_set_pin( GPIO_B, bit_21);	// OFF - 1 BLUE
-
-		GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT, bit_22); // OUTPUT - 1 RED
-		GPIO_data_direction_pin(GPIO_E, GPIO_OUTPUT, bit_26); // OUTPUT - 1 GREEN
-		GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT, bit_21); // OUTPUT - 1 BLUE
-}
 ////////////////////////////////////////////////////////////////////////////////
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
